801588c0178f123e1f43cc21e262866740e999ee
NeverLost-Never <haileyesus.mieron@gmail.com>
2023-02-17 15:48:40 -0800

    0x01-variables_if_else_while - 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints alphabets in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 9)
+	{
+		putchar('0' + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -24,3 +24,6 @@
 
 >>> 8-print_base16.c
 	- prints all the numbers of base 16 in lowercase, followed by a new line
+
+>>> 9-print_comb.c
+	- prints all possible combinations of single-digit numbers 
