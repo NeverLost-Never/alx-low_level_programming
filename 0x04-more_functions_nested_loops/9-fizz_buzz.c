a72ff239e305ce9276725911110bdb4212a5438e
NeverLost-Never <haileyesus.mieron@gmail.com>
2023-02-23 03:36:58 -0800

    0x04-more_functions_nested_loops - 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
