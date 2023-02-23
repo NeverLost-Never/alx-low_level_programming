8431726d790dddf7e59c1ef14317b343f0aabb45
NeverLost-Never <haileyesus.mieron@gmail.com>
2023-02-23 02:30:30 -0800

    0x04-more_functions_nested_loops - 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9,
+ * followed by a new line
+ * Return: always 0 (success)
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+		_putchar(c++);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,3 +6,6 @@
 
 >>> 2-mul.c
 	- multiplies two integers
+
+>>> 3-print_numbers.c
+	- prints the numbers, from 0 to 9, followed by a new line
